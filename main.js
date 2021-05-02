7acbff706e458d6aa89106c863e21fabbf0aca24
Zachary Rothstein <zrothstein@cs.uchicago.edu>
2021-05-02 00:41:14 -0500

    Seems to be working now, I am going to test with some test cases now

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
+var numbers = process.argv.slice(2);
+console.log('The Array: ', numbers);
+const DIGIT_MULTIPLIER = 10;
+var functionResult = intsToStrings(numbers);
+console.log("The Result: ", functionResult);
 
-
-var x = 11;
-var y = 2;
-var z = 1000;
-
-console.log("Expected: 2");
-console.log(getNumDigits(x));
-console.log("Expected: 1");
-console.log(getNumDigits(y));
-console.log("Expected: 4");
-console.log(getNumDigits(z));
 
 function intsToStrings(arr){
     var result = new Array(arr.length);
     var s;
-    var j = 1;
-    for (var i = 0; i < arr.length; i++){
-        while (arr[i] / j >= 1){
+    var j;
+    var entry;
+    var currentDigit;
 
-        }
+    for (var i = 0; i < arr.length; i++){
+        s = "";
+        j = 1;
+        entry = arr[i];
+        do {
+            currentDigit = Math.floor(entry) % Math.pow(DIGIT_MULTIPLIER, j); 
+            console.log("Current Digit: ", currentDigit);
+            s = digitToString(currentDigit) + s;
+            
+            j++;
+            entry /= 10;
+        } while (entry >= 1);
+        
+        result[i] = s;
+
     }
-    return 1;
+    return result;
 }
 /*
     getNumDigis: Calculates number of digits in an int
@@ -35,6 +43,47 @@ function getNumDigits(num){
     return digits;
 } 
 
-function getDigit(num, digitNumber){
-        
+function digitToString(digit){
+    var result;
+    switch (digit){
+        case 0: 
+            result = "Zero";
+            break;
+        case 1:
+            result = "One";
+            break;
+        case 2:
+            result = "Two";
+            break;
+        case 3:
+            result = "Three";
+            break;
+        case 4:
+            result = "Four";
+            break;
+        case 5:
+            result = "Five";
+            break;
+        case 6:
+            result = "Six";
+            break;
+        case 7:
+            result = "Seven";
+            break;
+        case 8:
+            result = "Eight";
+            break;
+        case 9:
+            result = "Nine";
+            break;   
+    }
+    
+    return result;
+}
+/*
+    Returns the kth digit of some integer.
+    Digit in the ones place is the "1st" integer
+*/
+function getDigit(number, k){
+    return number % Math.pow(10, k);
 }
