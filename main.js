0fc9a8c5253b03a89dc131bf660d31d67bdf429c
Zachary Rothstein <zrothstein@cs.uchicago.edu>
2021-05-02 01:49:18 -0500

    Optimized by replacing digitToString function with a table implemented as an array

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
 var numbers = process.argv.slice(2);
 const DIGIT_MULTIPLIER = 10;
+const digitToString = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", 
+"Seven", "Eight", "Nine"];
 var functionResult = intsToStrings(numbers);
 console.log(functionResult.toString());
-
 
 function intsToStrings(arr){
     var result = new Array(arr.length);
@@ -13,49 +14,14 @@ function intsToStrings(arr){
         var entry = arr[i];
         do {
             currentDigit = Math.floor(entry) % DIGIT_MULTIPLIER; 
-            s = digitToString(currentDigit) + s;
+            s = digitToString[currentDigit] + s;
             entry /= DIGIT_MULTIPLIER;
         } while (entry >= 1);
         result[i] = s;
     }
     return result;
 }
+ 
 
-function digitToString(digit){
-    var result;
-    switch (digit){
-        case 0: 
-            result = "Zero";
-            break;
-        case 1:
-            result = "One";
-            break;
-        case 2:
-            result = "Two";
-            break;
-        case 3:
-            result = "Three";
-            break;
-        case 4:
-            result = "Four";
-            break;
-        case 5:
-            result = "Five";
-            break;
-        case 6:
-            result = "Six";
-            break;
-        case 7:
-            result = "Seven";
-            break;
-        case 8:
-            result = "Eight";
-            break;
-        case 9:
-            result = "Nine";
-            break;    
-    }
-    
-    return result;
-}
+
 
